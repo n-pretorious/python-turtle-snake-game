009b39ff5fdad824d62f0dff18dfd3f13f69014c
pretorious <pretoriousndungu@gmail.com>
2023-07-24 20:47:17 +0300

    snake class

---
+++ snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 15
+STARTING_POSITIONS = [(0, 0), (-MOVE_DISTANCE, 0), (-2 * MOVE_DISTANCE, 0)]
+UP, DOWN, LEFT, RIGHT = 90, 270, 180, 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = self.create_segment(position)
+            self.segments.append(new_segment)
+
+    @staticmethod
+    def create_segment(position):
+        segment = Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        return segment
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        self.head.forward(MOVE_DISTANCE)
+
+    def set_direction(self, direction):
+        if self.head.heading() != direction:
+            self.head.setheading(direction)
+
+    def move_up(self):
+        self.set_direction(UP)
+
+    def move_down(self):
+        self.set_direction(DOWN)
+
+    def move_left(self):
+        self.set_direction(LEFT)
+
+    def move_right(self):
+        self.set_direction(RIGHT)
